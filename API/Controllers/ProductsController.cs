b9b044140fc6cae401887faeb7dd8ea643c51d58
Ezz Saeed <ezzsaid299@gmail.com>
2024-10-11 17:55:59 -0700

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Infrustructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var p = await _context.Products.ToListAsync();
+            return Ok(p);
+        }
+    }
+}
---
+++ Infrustructure/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Enitities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrustructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options):base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}

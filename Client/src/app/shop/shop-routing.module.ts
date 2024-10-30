d28b8c2ddc68edb6ff83340ca56f7176da1c4949
Ezz Saeed <ezzsaid299@gmail.com>
2024-10-29 22:06:41 -0700

    Adding redis, CustomerBasket and BAsketItems models and creating repository to get, update and delete a CustomerBasket

--- Client/src/app/shop/shop-routing.module.ts
+++ Client/src/app/shop/shop-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { provideRouter, RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
 import { ShopComponent } from './shop.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
---
+++ Core/Enitities/BasketItem.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.Enitities
+{
+    public class BasketItems
+    {
+    }
+}

f4a320389548d45509e3b6070e2acfe2a8f5268e
Ezz Saeed <ezzsaid299@gmail.com>
2024-11-17 21:33:12 -0800

    Dfining OrderService

---
+++ Core/Interfaces/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Enitities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string buerEmail, int deliveryMethod, string basketId, Address address);
+        Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buerEmail);
+        Task<Order> GetOrderByIdAsync(int id, string email);
+        Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
+    }
+}
---
+++ Infrustructure/Services/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Core.Enitities.OrderAggregate;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrustructure.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buerEmail, int deliveryMethod, string basketId, Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(int id, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
